e32f4766b3d89b5814a9bb4ec767bedd9b4d9399
Javohir Khamidullaev <javohir@greatsoft.uz>
2024-06-02 23:03:46 +0500

    Asosiy ishlari tugallandi.

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -2,12 +2,11 @@ from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandHelp
 
 from loader import dp
+from filters.prived_chat import IsPrivate
 
 
-@dp.message_handler(CommandHelp())
+@dp.message_handler(IsPrivate(), CommandHelp())
 async def bot_help(message: types.Message):
-    text = ("Buyruqlar: ",
-            "/start - Botni ishga tushirish",
-            "/help - Yordam")
-    
+    text = ("Buyruqlar: ", "/start - Botni ishga tushirish", "/help - Yordam")
+
     await message.answer("\n".join(text))
